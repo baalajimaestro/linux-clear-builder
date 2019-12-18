93426e4c8a09f559477e6d127d7daa27d8eb6c68
Baalaji <baalajimaestro@pixelexperience.org>
2019-12-18 23:53:17 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,11 +1,12 @@
-git clone https://github.com/baalajimaestro/linux-clear --depth=1 kernel-source
-cd kernel-source/ && scripts/install-git-hooks && curl -o linux-5.4.tar.xz http://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz && scripts/tar-up.sh
-rpmdev-setuptree
-rm -rf ~/rpmbuild/SOURCES
-mv ~/kernel-source/kernel-source ~/rpmbuild/SOURCES
-cd ~/rpmbuild/SOURCES/ && yes | rpmbuild -bb kernel-default.spec
 curl -sLo -u baalajimaestro:$GH_PERSONAL_TOKEN  ~/.ssh/id_ed25519 https://raw.githubusercontent.com/baalajimaestro/keys/master/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
+echo "SSH Keys Set!"
+rpmdev-setuptree
+rm -rf ~/rpmbuild/SOURCES
+git clone https://github.com/baalajimaestro/linux-clear --depth=1 kernel-source
+cd kernel-source/ && scripts/install-git-hooks && curl -o linux-5.4.tar.xz http://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz && scripts/tar-up.sh
+mv ~/kernel-source/kernel-source ~/rpmbuild/SOURCES
+cd ~/rpmbuild/SOURCES/ && yes | rpmbuild -bb kernel-default.spec
 for i in `ls ~/rpmbuild/RPMS/`
 do
 scp  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ~/rpmbuild/RPMS/$i baalaji20@storage.osdn.net:/storage/groups/b/ba/baalajimaestrobuilds/linux-clear/$i
