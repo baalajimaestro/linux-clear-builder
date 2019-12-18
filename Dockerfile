c72f0c35c853a1b95b3cb55d1fac17ffc724fc7b
baalajimaestro <baalajimaestro@pixelexperience.org>
2019-12-18 17:06:31 +0530

    Add upload to osdn
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- Dockerfile
+++ Dockerfile
@@ -21,10 +21,4 @@ RUN useradd runner --create-home
 USER runner
 WORKDIR /home/runner
 
-RUN git clone https://github.com/baalajimaestro/linux-clear --depth=1 kernel-source
-RUN cd kernel-source/ && scripts/install-git-hooks && curl -o linux-5.4.tar.xz http://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz && scripts/tar-up.sh
-RUN rpmdev-setuptree
-RUN rm -rf ~/rpmbuild/SOURCES
-RUN mv ~/kernel-source/kernel-source ~/rpmbuild/SOURCES
-RUN cd ~/rpmbuild/SOURCES/ && yes | rpmbuild -bb kernel-default.spec
-CMD ["bash"]
+CMD ["bash", "runner.sh"]
---
+++ runner.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/baalajimaestro/linux-clear --depth=1 kernel-source
+cd kernel-source/ && scripts/install-git-hooks && curl -o linux-5.4.tar.xz http://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz && scripts/tar-up.sh
+rpmdev-setuptree
+rm -rf ~/rpmbuild/SOURCES
+mv ~/kernel-source/kernel-source ~/rpmbuild/SOURCES
+cd ~/rpmbuild/SOURCES/ && yes | rpmbuild -bb kernel-default.spec
+echo $SSH_KEY > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+for i in `ls ~/rpmbuild/RPMS/`
+do
+scp -r ~/rpmbuild/RPMS/$i baalaji20@storage.osdn.net:/storage/groups/b/ba/baalajimaestrobuilds/linux-clear/$i
+done
